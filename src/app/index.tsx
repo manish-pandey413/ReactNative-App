faf237e81e32a46a9aa254e72372c23e306da75c
M-Xylene <manishpandey0.connect@gmail.com>
2025-07-07 00:50:31 +0530

    feat(UI): Card like container and header button.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { SectionList, StatusBar, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Drawer from "expo-router/drawer";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import useColorMode from "../hooks/useColorMode";
@@ -10,175 +10,159 @@ const DATA = [
   {
     title: 'Jan',
     data: [
-      {
-        itemName: 'Water',
-        price: 20
-      },
-
-      {
-        itemName: 'Coke',
-        price: 80
-      },
-
-      {
-        itemName: 'Beer',
-        price: 250
-      }
-
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Beer', price: 250 },
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Beer', price: 250 },
+      { itemName: 'Beer', price: 250 },
+      { itemName: 'Beer', price: 250 },
+      { itemName: 'Beer', price: 250 },
     ],
   },
+
   {
     title: 'Feb',
     data: [
-      {
-        itemName: 'Water',
-        price: 20
-      },
-
-      {
-        itemName: 'Coke',
-        price: 80
-      },
-
-      {
-        itemName: 'Beer',
-        price: 250
-      }
-
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Beer', price: 250 }
     ],
   },
+
   {
     title: 'Mar',
     data: [
-      {
-        itemName: 'Water',
-        price: 20
-      },
-
-      {
-        itemName: 'Coke',
-        price: 80
-      },
-
-      {
-        itemName: 'Beer',
-        price: 250
-      }
-
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Beer', price: 250 }
     ],
   },
+
   {
     title: 'Apr',
     data: [
-      {
-        itemName: 'Water',
-        price: 20
-      },
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Beer', price: 250 }
+    ],
+  },
 
-      {
-        itemName: 'Coke',
-        price: 80
-      },
 
-      {
-        itemName: 'Beer',
-        price: 250
-      }
-
+  {
+    title: 'May',
+    data: [
+      { itemName: 'Water', price: 20 },
+      { itemName: 'Coke', price: 80 },
+      { itemName: 'Beer', price: 250 }
     ],
   },
 ];
 
 
-export default function Index() {
+export default function Home() {
 
-  const bgColor: string = useColorMode("#09090a", "#f5f5ff");
+  const bgColor: string = useColorMode("#09090aef", "#FFFFFF0D");
+  const cardColor: string = useColorMode("#111", "#fff");
   const statusTheme: StatusBarStyle = useColorMode("light-content", "dark-content") as StatusBarStyle;
-  const fontColor = useColorMode("#f5f5f9", "#09090a");
+  const fontColor: string = useColorMode("#f5f5f9", "#09090a");
 
   const [modalVisible, setModalVisible] = useState(false);
-
   const [number, onChangeNumber] = useState(0);
-
-  const dimensions = useWindowDimensions();
 
 
   return (
-    <View style={{ ...styles.view, backgroundColor: bgColor }}>
+    <View style={[styles.mainView, { backgroundColor: bgColor }]}>
       <Drawer.Screen
         options={{
           headerStyle: {
-            backgroundColor: bgColor
+            backgroundColor: bgColor,
           },
+          headerShadowVisible: false,
           drawerLabel: 'Home',
-          title: `Welcome!  Guest`,
+          title: 'UNotes',
           headerTitleStyle: {
-            fontSize: 24,
+            fontSize: 28,
           },
-          drawerIcon: () => <AntDesign name="home" size={30} color={fontColor} />
+          drawerIcon: () => <AntDesign name="home" size={30} color={fontColor} />,
+          headerRight: () => <TouchableOpacity
+            style={styles.addBtn}
+            onPress={() => { setModalVisible(true) }}
+          >
+
+            <Text style={{ fontSize: 19, color: "#481fc4" }}>Add Amount</Text>
+
+          </TouchableOpacity>
         }}
       />
       <StatusBar barStyle={statusTheme} />
 
       <AddItemModal modalVisible={modalVisible} setModalVisible={setModalVisible} onChangeNumber={onChangeNumber} number={number} />
 
-      <SectionList
-        sections={DATA}
-        keyExtractor={(item, index) => item.itemName + index}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item}>
-            <Text style={styles.title}>{item.itemName}</Text>
-            <Text style={styles.price}>{`$ ${item.price} /-`}</Text>
-          </TouchableOpacity>
-        )}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text style={styles.header}>{title}</Text>
-        )}
-      />
+      <ScrollView style={{ padding: 18 }}>
+        {DATA.map((section) => (
+          <View
+            key={section.title}
+            style={[styles.sectionContainer, { backgroundColor: cardColor },]}
+          >
+            <Text style={[styles.sectionHeader, { color: fontColor, backgroundColor: cardColor }]}>{section.title}</Text>
+            <View style={{ backgroundColor: 'gray', height: 0.5 }} />
 
-      <View style={{ position: 'absolute', left: dimensions.width - 100, top: dimensions.height - 215 }}>
-        <AntDesign.Button name="pluscircleo" size={55} color={"#00db9d"} backgroundColor={"transparent"} underlayColor={"transparent"}
-          onPress={() => {
-            setModalVisible(true)
-          }}>
-        </AntDesign.Button>
-      </View >
+            {section.data.map((item, index) => (
+              <TouchableOpacity key={index} style={styles.itemRow}>
+                <Text style={styles.itemText}>{item.itemName}</Text>
+                <Text style={styles.priceText}>₹ {item.price} /-</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+
     </View >
   );
 }
 
 const styles = StyleSheet.create({
-  view: {
+  mainView: {
     padding: 1
     , flex: 1
   },
-  item: {
-    flex: 1
-    , flexDirection: 'row'
-    , padding: 10
-    , margin: 9
-    , borderRadius: 12
-  },
-  header: {
-    fontSize: 28
-    , padding: 5
-    , paddingLeft: 15
-    , backgroundColor: '#282828'
-    , marginTop: 12
-    , margin: 2
-    , color: '#f5f5ff'
-  },
-  title: {
-    fontSize: 21
-    , flex: 1
-    , flexDirection: 'row'
-    , color: '#f5f5ff'
-  },
-  price: {
-    fontSize: 21
-    , paddingRight: 10
-    , justifyContent: 'flex-end'
-    , color: '#f5f5ff'
+  addBtn: {
+    backgroundColor: "#5633bf30"
+    , marginRight: 10
+    , paddingHorizontal: 24
+    , paddingVertical: 7
+    , borderRadius: 20
   },
 
+  sectionContainer: {
+    shadowColor: '#000',
+    elevation: 8,
+    marginVertical: 15,
+    marginHorizontal: 5,
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  sectionHeader: {
+    fontSize: 30,
+    padding: 15,
+  },
+  itemRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 18,
+    paddingHorizontal: 32,
+    margin: 5
+  },
+  itemText: {
+    fontSize: 18,
+    color: '#fff',
+  },
+  priceText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '600',
+  },
 });
